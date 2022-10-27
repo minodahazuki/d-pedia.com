aee51f1c86676bd0ba1fc35e1f15a67d75ac30db
Nancy <52145579+aLiz-Nancy@users.noreply.github.com>
2022-10-27 17:31:30 +0900

    feat(settings): update nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,17 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  head: {},
 
   css: [
     'destyle.css',
   ],
+
+  modules: [],
+
+  srcDir: 'app/',
+
+  typescript: {
+    strict: true,
+  },
+
 });
